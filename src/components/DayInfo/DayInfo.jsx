234e939218b32677dc66382fab9db0ccf3e9b82d
Vladislav Kutsevich <kutsevichvlad18@gmail.com>
2025-10-26 19:55:23 +0200

    Add changes

--- src/components/DayInfo/DayInfo.jsx
+++ src/components/DayInfo/DayInfo.jsx
@@ -11,35 +11,35 @@ const DayInfo = ({ forecast }) => {
     <DayInfoWrapper>
       <DayInfoList>
         <DayItem>
-          <DayItemLabel>Восход</DayItemLabel>
+          <DayItemLabel>Sunrise</DayItemLabel>
           <DayItemValue>
             {forecast.forecast.forecastday[0].astro.sunrise}
           </DayItemValue>
         </DayItem>
         <DayItem>
-          <DayItemLabel>Скорость ветра</DayItemLabel>
+          <DayItemLabel>Wind Speed</DayItemLabel>
           <DayItemValue>
             {forecast.forecast.forecastday[0].day.maxwind_kph} км/ч
           </DayItemValue>
         </DayItem>
         <DayItem>
-          <DayItemLabel>Давление</DayItemLabel>
+          <DayItemLabel>Pressure</DayItemLabel>
           <DayItemValue> {forecast.current.pressure_mb} hPa </DayItemValue>
         </DayItem>
         <DayItem>
-          <DayItemLabel>Закат</DayItemLabel>
+          <DayItemLabel>Sunset</DayItemLabel>
           <DayItemValue>
             {forecast.forecast.forecastday[0].astro.sunset}
           </DayItemValue>
         </DayItem>
         <DayItem>
-          <DayItemLabel>Вероятность дождя</DayItemLabel>
+          <DayItemLabel>Chance of Rain</DayItemLabel>
           <DayItemValue>
             {forecast.forecast.forecastday[0].day.daily_chance_of_rain} %
           </DayItemValue>
         </DayItem>
         <DayItem>
-          <DayItemLabel>УФ индекс</DayItemLabel>
+          <DayItemLabel>UV Index</DayItemLabel>
           <DayItemValue>{forecast.forecast.forecastday[0].day.uv}</DayItemValue>
         </DayItem>
       </DayInfoList>
--- src/components/HourlyForecastCard/HourlyForecastCard.styled.jsx
+++ src/components/HourlyForecastCard/HourlyForecastCard.styled.jsx
@@ -1,21 +1,18 @@
 import styled from '@emotion/styled';
 
 export const HourlyCard = styled.li`
-  // flex: 0 0 120px; /* фиксированная ширина карточки */
   display: flex;
   flex-direction: column;
   align-items: center;
   text-align: center;
   padding: 10px;
-  border-radius: 8px;
-  background: #f5f5f5; /* можно заменить на прозрачный или по макету */
 `;
 
 export const HourLabel = styled.span`
   font-weight: 500;
   font-size: 14px;
   margin-bottom: 5px;
-  color: #ffffffff;
+  color: #f5f5f5;
 `;
 
 export const HourIcon = styled.img`
@@ -27,5 +24,5 @@ export const HourIcon = styled.img`
 export const HourTemp = styled.span`
   font-weight: 600;
   font-size: 16px;
-  color: #ffffffff;
+  color: #f5f5f5;
 `;
