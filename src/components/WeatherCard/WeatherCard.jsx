75c9bbde138e9ed09875663102d74a0d76e19fa8
Vladislav Kutsevich <kutsevichvlad18@gmail.com>
2025-11-05 16:17:28 +0200

    add list and styled components

--- src/components/WeatherCard/WeatherCard.jsx
+++ src/components/WeatherCard/WeatherCard.jsx
@@ -1,30 +1,41 @@
 import {
   Card,
-  // Image,
-  Content,
-  Title,
-  Type,
-  Abilities,
-  // StatsList,
-  // StatsListItem,
-  // Span,
+  Left,
+  City,
+  Country,
+  Temp,
+  FeelsLike,
+  Right,
+  Icon,
+  Condition,
 } from './WeatherCard.styled';
 
 // import capitalizeFirstLetter from '../../utils/capitalizeFirstLetter';
 
-const PokemonCard = () => {
+const WeatherCard = ({ cityWeather }) => {
+  console.log(cityWeather);
   return (
     <>
-      <Card>
-        <Title>Kiev</Title>
+      {cityWeather.map(city => (
+        <Card key={city.location.name}>
+          <Left>
+            <City>{city.location.name}</City>
+            <Country>{city.location.country}</Country>
+            <Temp>{city.current.temp_c}°C</Temp>
+            <FeelsLike>Feels like:{city.current.feelslike_c}°C</FeelsLike>
+          </Left>
 
-        <Content>
-          <Type>Type:</Type>
-          <Abilities>Abilities:</Abilities>
-        </Content>
-      </Card>
+          <Right>
+            <Icon
+              src={city.current.condition.icon}
+              alt={city.current.condition.text}
+            />
+            <Condition>{city.current.condition.text}</Condition>
+          </Right>
+        </Card>
+      ))}
     </>
   );
 };
 
-export default PokemonCard;
+export default WeatherCard;
--- src/components/WeatherCard/WeatherCard.styled.jsx
+++ src/components/WeatherCard/WeatherCard.styled.jsx
@@ -1,80 +1,68 @@
 import styled from '@emotion/styled';
 
-export const Card = styled.li`
-  background: linear-gradient(to bottom, #2c2c2c, #1e1e1e);
-  border-radius: 10px;
-  overflow: hidden;
-  box-shadow: 0 4px 8px rgba(0, 0, 0, 0.1);
-  width: 250px;
-  height: 400px;
-  text-align: center;
-  transition: transform 0.3s ease-in-out;
+export const Card = styled.div`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  background-color: #1e213a;
+  color: #fff;
+  padding: 20px;
+  border-radius: 16px;
+  width: 220px;
+  height: 200px;
+    transition: all 0.3s ease;
+  box-shadow: 0 2px 6px rgba(0, 0, 0, 0.2);
 
   &:hover {
     transform: scale(1.05);
-  }
+    box-shadow: 0 8px 20px rgba(0, 0, 0, 0.4);
+    background-color: #232643;
 `;
 
-export const Image = styled.img`
-  width: 200px;
-  height: 150px;
-  object-fit: cover;
-`;
-
-export const Content = styled.div`
-  border: 1px solid black;
-  border-radius: 10px;
-  background-color: #f5deb3;
-  margin: 10px;
-
-  padding: 10px;
-`;
-
-export const Title = styled.h3`
-  margin-bottom: 0px;
-  margin-top: 10px;
-
-  font-size: 25px;
-  color: #fcfbfbff;
-`;
-
-export const Type = styled.h3`
-  font-size: 16px;
-  color: #333;
-  margin-bottom: 0px;
-  margin-top: 10px;
-`;
-
-export const Abilities = styled.h3`
-  font-size: 16px;
-  color: #333;
-  margin-bottom: 0px;
-  margin-top: 10px;
-`;
-export const Stats = styled.h3`
-  font-size: 16px;
-  color: #333;
-  margin-bottom: 10px;
-  margin-top: 0px;
-`;
-
-export const StatsList = styled.ul`
-  list-style-type: none;
-  padding: 0;
-  margin: 10px 0;
-  width: 100%;
-`;
-
-export const StatsListItem = styled.li`
+export const Left = styled.div`
   display: flex;
-  justify-content: flex-start;
-  margin-bottom: 6px;
-  font-size: 14px;
-  font-weight: 400;
+  flex-direction: column;
+  justify-content: space-between;
 `;
 
-export const Span = styled.span`
-  color: #333;
-  font-weight: 400;
-  margin-left: 5px;
+export const City = styled.h3`
+  margin-top: 0px;
+  font-size: 22px;
+  font-weight: 600;
+`;
+
+export const Country = styled.p`
+  margin-top: 0px;
+  font-size: 14px;
+`;
+
+export const Temp = styled.p`
+  margin-top: 0px;
+  margin-bottom: 10px;
+  font-size: 32px;
+  font-weight: bold;
+`;
+
+export const FeelsLike = styled.p`
+  margin-top: 0px;
+  margin-bottom: 0px;
+  font-size: 13px;
+`;
+
+export const Right = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+`;
+
+export const Icon = styled.img`
+  width: 60px;
+  height: 60px;
+`;
+
+export const Condition = styled.p`
+  text-align: center;
+  margin-top: 0px;
+  font-size: 14px;
+  margin-top: 6px;
 `;
